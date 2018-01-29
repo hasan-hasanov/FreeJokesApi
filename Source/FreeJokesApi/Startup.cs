20d79b5d66bdd19a8e1954025185b69bf69406a0
Hasan Hasanov <hasan.mehmedov@gmail.com>
2018-01-30 00:48:25 +0200

    Register services.

--- Source/FreeJokesApi/Startup.cs
+++ Source/FreeJokesApi/Startup.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Application.Abstract;
+using Application.Jokes.Queries.GetJokesWithCategories;
+using Application.Jokes.Queries.GetJokesWithCategories.Abstract;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Persistence;
+using Persistence.Seralizers;
+using Persistence.Serializers.Abstract;
 
 namespace FreeJokesApi
 {
@@ -20,13 +20,16 @@ namespace FreeJokesApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddScoped<IJokeService, JokeService>();
+            services.AddScoped<IGetJokesWithCategoriesQuery, GetJokesWithCategoriesQuery>();
+
+            services.AddScoped<IDataSerializer, DataSerializer>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
