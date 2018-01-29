8bb3b569eb34b77e9f6502fad0b2f18874bc002b
Hasan Hasanov <hasan.mehmedov@gmail.com>
2018-01-29 23:44:37 +0200

    Domain layer added.

---
+++ Source/Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Category : IEntity<int>
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Source/Domain/Entities/Joke.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Joke : IEntity<string>
+    {
+        public string Id { get; set; }
+
+        public string Description { get; set; }
+
+        public int Category { get; set; }
+    }
+}
