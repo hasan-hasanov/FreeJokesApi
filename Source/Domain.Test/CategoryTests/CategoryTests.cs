bbca267750a3276fbc37708f6ded14b2ba7bced0
Hasan Hasanov <hasan.mehmedov@gmail.com>
2018-02-05 23:32:43 +0200

    Domain Category tests.

---
+++ Source/Domain.Test/CategoryTests/CategoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using NUnit.Framework;
+
+namespace Domain.Test.CategoryTests
+{
+    public class CategoryTests
+    {
+        private readonly Category _category;
+
+        private const int Id = 1;
+        private const string Description = "Test";
+
+
+        public CategoryTests()
+        {
+            _category = new Category();
+        }
+
+        [Test]
+        public void TestSetAndGetId()
+        {
+            _category.Id = Id;
+
+            Assert.That(_category.Id, Is.EqualTo(Id));
+        }
+
+        [Test]
+        public void TestSetAndGetName()
+        {
+            _category.Description = Description;
+
+            Assert.That(_category.Description, Is.EqualTo(Description));
+        }
+    }
+}
